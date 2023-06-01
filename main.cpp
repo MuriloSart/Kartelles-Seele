9264e90563b546f0ab329725a2cc5900e5c885da
Murilo Sartori <89427588+MuriloSart@users.noreply.github.com>
2023-05-31 23:53:53 -0300

    Colocando os cenários em um vetor de vetores e setando os novos itens

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ typedef struct _blocosColisao
 
 //criando o vetor para blocos de colisão
 BlocoDeColisao *blocosColisao;
-int qntBlocos = 27;
+int qntBlocos = 30;
 
 //checagem de itens coletados
 int qntItensColetados = 0;
@@ -187,16 +187,214 @@ int main()
   blocosColisao[9].sprite = NULL;
   blocosColisao[9].spriteMascara = NULL;
   
-  blocosColisao[9].x = 1100;
-  blocosColisao[9].y = 130;
-  blocosColisao[9].altura = 64;
-  blocosColisao[9].largura = 64;
-  blocosColisao[9].tipo = 5;
-  blocosColisao[9].colidido = false;
-  blocosColisao[9].cliqueMouse = false;
-  blocosColisao[9].coletado = false;
-  blocosColisao[9].sprite = NULL;
-  blocosColisao[9].spriteMascara = NULL;
+  blocosColisao[10].x = 1100;
+  blocosColisao[10].y = 130;
+  blocosColisao[10].altura = 64;
+  blocosColisao[10].largura = 64;
+  blocosColisao[10].tipo = 5;
+  blocosColisao[10].colidido = false;
+  blocosColisao[10].cliqueMouse = false;
+  blocosColisao[10].coletado = false;
+  blocosColisao[10].sprite = NULL;
+  blocosColisao[10].spriteMascara = NULL;
+  
+  blocosColisao[11].x = 1100;
+  blocosColisao[11].y = 130;
+  blocosColisao[11].altura = 64;
+  blocosColisao[11].largura = 64;
+  blocosColisao[11].tipo = 5;
+  blocosColisao[11].colidido = false;
+  blocosColisao[11].cliqueMouse = false;
+  blocosColisao[11].coletado = false;
+  blocosColisao[11].sprite = NULL;
+  blocosColisao[11].spriteMascara = NULL;
+  
+  blocosColisao[12].x = 1100;
+  blocosColisao[12].y = 130;
+  blocosColisao[12].altura = 64;
+  blocosColisao[12].largura = 64;
+  blocosColisao[12].tipo = 5;
+  blocosColisao[12].colidido = false;
+  blocosColisao[12].cliqueMouse = false;
+  blocosColisao[12].coletado = false;
+  blocosColisao[12].sprite = NULL;
+  blocosColisao[12].spriteMascara = NULL;
+  
+  blocosColisao[13].x = 1100;
+  blocosColisao[13].y = 130;
+  blocosColisao[13].altura = 64;
+  blocosColisao[13].largura = 64;
+  blocosColisao[13].tipo = 5;
+  blocosColisao[13].colidido = false;
+  blocosColisao[13].cliqueMouse = false;
+  blocosColisao[13].coletado = false;
+  blocosColisao[13].sprite = NULL;
+  blocosColisao[13].spriteMascara = NULL;
+  
+  blocosColisao[13].x = 1100;
+  blocosColisao[13].y = 130;
+  blocosColisao[13].altura = 64;
+  blocosColisao[13].largura = 64;
+  blocosColisao[13].tipo = 5;
+  blocosColisao[13].colidido = false;
+  blocosColisao[13].cliqueMouse = false;
+  blocosColisao[13].coletado = false;
+  blocosColisao[13].sprite = NULL;
+  blocosColisao[13].spriteMascara = NULL;
+  
+  blocosColisao[14].x = 1100;
+  blocosColisao[14].y = 130;
+  blocosColisao[14].altura = 64;
+  blocosColisao[14].largura = 64;
+  blocosColisao[14].tipo = 5;
+  blocosColisao[14].colidido = false;
+  blocosColisao[14].cliqueMouse = false;
+  blocosColisao[14].coletado = false;
+  blocosColisao[14].sprite = NULL;
+  blocosColisao[14].spriteMascara = NULL;
+  
+  blocosColisao[15].x = 1100;
+  blocosColisao[15].y = 130;
+  blocosColisao[15].altura = 64;
+  blocosColisao[15].largura = 64;
+  blocosColisao[15].tipo = 5;
+  blocosColisao[15].colidido = false;
+  blocosColisao[15].cliqueMouse = false;
+  blocosColisao[15].coletado = false;
+  blocosColisao[15].sprite = NULL;
+  blocosColisao[15].spriteMascara = NULL;
+  
+  blocosColisao[16].x = 1100;
+  blocosColisao[16].y = 130;
+  blocosColisao[16].altura = 64;
+  blocosColisao[16].largura = 64;
+  blocosColisao[16].tipo = 5;
+  blocosColisao[16].colidido = false;
+  blocosColisao[16].cliqueMouse = false;
+  blocosColisao[16].coletado = false;
+  blocosColisao[16].sprite = NULL;
+  blocosColisao[16].spriteMascara = NULL;
+  
+  blocosColisao[17].x = 1100;
+  blocosColisao[17].y = 130;
+  blocosColisao[17].altura = 64;
+  blocosColisao[17].largura = 64;
+  blocosColisao[17].tipo = 5;
+  blocosColisao[17].colidido = false;
+  blocosColisao[17].cliqueMouse = false;
+  blocosColisao[17].coletado = false;
+  blocosColisao[17].sprite = NULL;
+  blocosColisao[17].spriteMascara = NULL;
+  
+  blocosColisao[18].x = 1100;
+  blocosColisao[18].y = 130;
+  blocosColisao[18].altura = 64;
+  blocosColisao[18].largura = 64;
+  blocosColisao[18].tipo = 5;
+  blocosColisao[18].colidido = false;
+  blocosColisao[18].cliqueMouse = false;
+  blocosColisao[18].coletado = false;
+  blocosColisao[18].sprite = NULL;
+  blocosColisao[18].spriteMascara = NULL;
+  
+  blocosColisao[19].x = 1100;
+  blocosColisao[19].y = 130;
+  blocosColisao[19].altura = 64;
+  blocosColisao[19].largura = 64;
+  blocosColisao[19].tipo = 5;
+  blocosColisao[19].colidido = false;
+  blocosColisao[19].cliqueMouse = false;
+  blocosColisao[19].coletado = false;
+  blocosColisao[19].sprite = NULL;
+  blocosColisao[19].spriteMascara = NULL;
+  
+  blocosColisao[20].x = 1100;
+  blocosColisao[20].y = 130;
+  blocosColisao[20].altura = 64;
+  blocosColisao[20].largura = 64;
+  blocosColisao[20].tipo = 5;
+  blocosColisao[20].colidido = false;
+  blocosColisao[20].cliqueMouse = false;
+  blocosColisao[20].coletado = false;
+  blocosColisao[20].sprite = NULL;
+  blocosColisao[20].spriteMascara = NULL;
+  
+  blocosColisao[21].x = 1100;
+  blocosColisao[21].y = 130;
+  blocosColisao[21].altura = 64;
+  blocosColisao[21].largura = 64;
+  blocosColisao[21].tipo = 5;
+  blocosColisao[21].colidido = false;
+  blocosColisao[21].cliqueMouse = false;
+  blocosColisao[21].coletado = false;
+  blocosColisao[21].sprite = NULL;
+  blocosColisao[21].spriteMascara = NULL;
+  
+  blocosColisao[22].x = 1100;
+  blocosColisao[22].y = 130;
+  blocosColisao[22].altura = 64;
+  blocosColisao[22].largura = 64;
+  blocosColisao[22].tipo = 5;
+  blocosColisao[22].colidido = false;
+  blocosColisao[22].cliqueMouse = false;
+  blocosColisao[22].coletado = false;
+  blocosColisao[22].sprite = NULL;
+  blocosColisao[22].spriteMascara = NULL;
+  
+  blocosColisao[23].x = 1100;
+  blocosColisao[23].y = 130;
+  blocosColisao[23].altura = 64;
+  blocosColisao[23].largura = 64;
+  blocosColisao[23].tipo = 5;
+  blocosColisao[23].colidido = false;
+  blocosColisao[23].cliqueMouse = false;
+  blocosColisao[23].coletado = false;
+  blocosColisao[23].sprite = NULL;
+  blocosColisao[23].spriteMascara = NULL;
+  
+  blocosColisao[24].x = 1100;
+  blocosColisao[24].y = 130;
+  blocosColisao[24].altura = 64;
+  blocosColisao[24].largura = 64;
+  blocosColisao[24].tipo = 5;
+  blocosColisao[24].colidido = false;
+  blocosColisao[24].cliqueMouse = false;
+  blocosColisao[24].coletado = false;
+  blocosColisao[24].sprite = NULL;
+  blocosColisao[24].spriteMascara = NULL;
+  
+  blocosColisao[25].x = 1100;
+  blocosColisao[25].y = 130;
+  blocosColisao[25].altura = 64;
+  blocosColisao[25].largura = 64;
+  blocosColisao[25].tipo = 5;
+  blocosColisao[25].colidido = false;
+  blocosColisao[25].cliqueMouse = false;
+  blocosColisao[25].coletado = false;
+  blocosColisao[25].sprite = NULL;
+  blocosColisao[25].spriteMascara = NULL;
+  
+  blocosColisao[26].x = 1100;
+  blocosColisao[26].y = 130;
+  blocosColisao[26].altura = 64;
+  blocosColisao[26].largura = 64;
+  blocosColisao[26].tipo = 5;
+  blocosColisao[26].colidido = false;
+  blocosColisao[26].cliqueMouse = false;
+  blocosColisao[26].coletado = false;
+  blocosColisao[26].sprite = NULL;
+  blocosColisao[26].spriteMascara = NULL;
+  
+  blocosColisao[27].x = 1100;
+  blocosColisao[27].y = 130;
+  blocosColisao[27].altura = 64;
+  blocosColisao[27].largura = 64;
+  blocosColisao[27].tipo = 5;
+  blocosColisao[27].colidido = false;
+  blocosColisao[27].cliqueMouse = false;
+  blocosColisao[27].coletado = false;
+  blocosColisao[27].sprite = NULL;
+  blocosColisao[27].spriteMascara = NULL;
   
   //personagens
   //=>Lontra sem outline
@@ -229,41 +427,33 @@ int main()
   bool inventario = true;
   
   //===============================> Cenários <===============================
-  void *cenario1;
-  cenario1 = carregarImagem(".//Artes//Cenarios//sala_tutas.bmp", 1280, 600, 0, 0);
+  void **cenarios;
+  int qntDeCenarios = 12;
+  cenarios = (void **) malloc(sizeof(void *) * qntDeCenarios);
+
+  cenarios[0] = carregarImagem(".//Artes//Cenarios//sala_tutas.bmp", 1280, 600, 0, 0);
   
-  void *cenario2;
-  cenario2 = carregarImagem(".//Artes//Cenarios//quarto_tutas.bmp", 1280, 600, 0, 0);
+  cenarios[1] = carregarImagem(".//Artes//Cenarios//quarto_tutas.bmp", 1280, 600, 0, 0);
   
-  void *cenario3;
-  cenario3 = carregarImagem(".//Artes//Cenarios//fase_2_cenario_1.bmp", 1280, 600, 0, 0);
+  cenarios[2] = carregarImagem(".//Artes//Cenarios//fase_2_cenario_1.bmp", 1280, 600, 0, 0);
   
-  void *cenario4;
-  cenario4 = carregarImagem(".//Artes//Cenarios//fase_2_cenario_2.bmp", 1280, 600, 0, 0);
+  cenarios[3] = carregarImagem(".//Artes//Cenarios//fase_2_cenario_2.bmp", 1280, 600, 0, 0);
   
-  void *cenario5;
-  cenario5 = carregarImagem(".//Artes//Cenarios//fase_2_cenario_3.bmp", 1280, 600, 0, 0);
+  cenarios[4] = carregarImagem(".//Artes//Cenarios//fase_2_cenario_3.bmp", 1280, 600, 0, 0);
   
-  void *cenario6;
-  cenario6 = carregarImagem(".//Artes//Cenarios//fase_2_cenario_4.bmp", 1280, 600, 0, 0);
+  cenarios[5] = carregarImagem(".//Artes//Cenarios//fase_2_cenario_4.bmp", 1280, 600, 0, 0);
   
-  void *cenario7;
-  cenario7 = carregarImagem(".//Artes//Cenarios//fase_2_cenario_5.bmp", 1280, 600, 0, 0);
+  cenarios[6] = carregarImagem(".//Artes//Cenarios//fase_2_cenario_5.bmp", 1280, 600, 0, 0);
   
-  void *cenario8;
-  cenario8 = carregarImagem(".//Artes//Cenarios//fase_2_cenario_6.bmp", 1280, 600, 0, 0);
+  cenarios[7] = carregarImagem(".//Artes//Cenarios//fase_2_cenario_6.bmp", 1280, 600, 0, 0);
   
-  void *cenario9;
-  cenario9 = carregarImagem(".//Artes//Cenarios//fase_2_cenario_7.bmp", 1280, 600, 0, 0);
+  cenarios[8] = carregarImagem(".//Artes//Cenarios//fase_2_cenario_7.bmp", 1280, 600, 0, 0);
   
-  void *cenario10;
-  cenario10 = carregarImagem(".//Artes//Cenarios//fase_2_cenario_8.bmp", 1280, 600, 0, 0);
+  cenarios[9] = carregarImagem(".//Artes//Cenarios//fase_2_cenario_8.bmp", 1280, 600, 0, 0);
   
-  void *cenario11;
-  cenario11 = carregarImagem(".//Artes//Cenarios//fase_2_cenario_9.bmp", 1280, 600, 0, 0);
+  cenarios[10] = carregarImagem(".//Artes//Cenarios//fase_2_cenario_9.bmp", 1280, 600, 0, 0);
   
-  void *cenario12;
-  cenario12 = carregarImagem(".//Artes//Cenarios//fase_2_cenario_10.bmp", 1280, 600, 0, 0);
+  cenarios[11] = carregarImagem(".//Artes//Cenarios//fase_2_cenario_10.bmp", 1280, 600, 0, 0);
   
   //=======================> Salvando o tick do computador Inicialmente <=======================
   gt1 = GetTickCount();//registrando o tick inicial do computador para intervalo de frames
@@ -285,7 +475,7 @@ int main()
       //=================> Lidando com a troca de fases <=================
       if(fases == 1)
       {
-        LidandoComFases(cenario1, qntItensColetados, fases, false, false, true, false, inventario, 5, 6, 5, 9);
+        LidandoComFases(cenarios[0], qntItensColetados, fases, false, false, true, false, inventario, 5, 6, 5, 9);
         if(qntItensColetados >= 5)
         {
 	      inventario = false;
@@ -294,7 +484,7 @@ int main()
 	  }//não há segunda fase pela lógica que criei de transição de fase
 	  else if(fases == 3)
 	  {
-	    LidandoComFases(cenario2, qntItensColetados, fases, false, false, false, true, inventario, 7, 9, 5, 9);
+	    LidandoComFases(cenarios[1], qntItensColetados, fases, false, false, false, true, inventario, 7, 9, 5, 9);
 	    if(qntItensColetados >= 5)
 	    {
 	      inventario = false;
@@ -303,49 +493,49 @@ int main()
 	  }
 	  else if(fases == 4)
 	  {
-	    LidandoComFases(cenario3, qntItensColetados, fases, false, false, true, false, inventario, 7, 9, 5, 9);
-	    if(!blocosColisao[10].colidido)
-	      DesenhandoBotao( blocosColisao[25].x, blocosColisao[25].y, blocosColisao[25].sprite, blocosColisao[25].spriteMascara);
+	    LidandoComFases(cenarios[2], qntItensColetados, fases, false, false, true, false, inventario, 7, 9, 5, 9);
+	    if(!blocosColisao[28].colidido)
+	      DesenhandoBotao( blocosColisao[28].x, blocosColisao[28].y, blocosColisao[28].sprite, blocosColisao[28].spriteMascara);
 	    else
-	      DesenhandoBotao( blocosColisao[26].x, blocosColisao[26].y, blocosColisao[26].sprite, blocosColisao[26].spriteMascara);
+	      DesenhandoBotao( blocosColisao[29].x, blocosColisao[29].y, blocosColisao[29].sprite, blocosColisao[29].spriteMascara);
 	  }
 	  else if(fases == 6)
 	  {
-	    LidandoComFases(cenario4, qntItensColetados, fases, true, false, pegouMissao, true, inventario, 7, 9, 5, 9);
+	    LidandoComFases(cenarios[3], qntItensColetados, fases, true, false, pegouMissao, true, inventario, 7, 9, 5, 9);
 	  }
 	  else if(fases == 7)
 	  {
-	    LidandoComFases(cenario5, qntItensColetados, fases, false, true, true, false, inventario, 7, 9, 5, 9);
+	    LidandoComFases(cenarios[4], qntItensColetados, fases, false, true, true, false, inventario, 7, 9, 5, 9);
 	  }
 	  else if(fases == 8)
 	  {
-	    LidandoComFases(cenario7, qntItensColetados, fases, false, false, true, true, inventario, 7, 9, 5, 9);
+	    LidandoComFases(cenarios[6], qntItensColetados, fases, false, false, true, true, inventario, 7, 9, 5, 9);
 	  }
 	  else if(fases == 9)
 	  {
-	    LidandoComFases(cenario6, qntItensColetados, fases, false, false, false, true, inventario, 7, 9, 5, 9);
+	    LidandoComFases(cenarios[5], qntItensColetados, fases, false, false, false, true, inventario, 7, 9, 5, 9);
 	    pegouMissao = true;
 	  }
 	  else if(fases == 10)
 	  {
-	    LidandoComFases(cenario8, qntItensColetados, fases, false, false, true, true, inventario, 7, 9, 5, 9);
+	    LidandoComFases(cenarios[7], qntItensColetados, fases, false, false, true, true, inventario, 7, 9, 5, 9);
 	    inventario = true;
 	  }
 	  else if(fases == 12)
 	  {
-	    LidandoComFases(cenario9, qntItensColetados, fases, true, false, true, true, inventario, 7, 9, 5, 9);
+	    LidandoComFases(cenarios[8], qntItensColetados, fases, true, false, true, true, inventario, 7, 9, 5, 9);
 	  }
 	  else if(fases == 13)
 	  {
-	    LidandoComFases(cenario10, qntItensColetados, fases, false, true, true, false, inventario, 7, 9, 5, 9);
+	    LidandoComFases(cenarios[9], qntItensColetados, fases, false, true, true, false, inventario, 7, 9, 5, 9);
 	  }
 	  else if(fases == 14)
 	  {
-	    LidandoComFases(cenario12, qntItensColetados, fases, false, false, false, true, inventario, 7, 9, 5, 9);
+	    LidandoComFases(cenarios[11], qntItensColetados, fases, false, false, false, true, inventario, 7, 9, 5, 9);
 	  }
 	  else if(fases == 15)
 	  {
-	    LidandoComFases(cenario11, qntItensColetados, fases, false, false, false, true, inventario, 7, 9, 5, 9);
+	    LidandoComFases(cenarios[10], qntItensColetados, fases, false, false, false, true, inventario, 7, 9, 5, 9);
 	  }
 	  
       setvisualpage(pg);
@@ -360,24 +550,20 @@ int main()
   }
   
   printf("\n\nFim do Programa");
-  for(int i = 0; i<qntBlocos; i++)
+  
+  for(int i = 0; i<qntBlocos; i++)//liberando memória dos blocos
   {
     free(blocosColisao[i].sprite);
   	free(blocosColisao[i].spriteMascara);
   }
   free(blocosColisao);
-  free(cenario1);
-  free(cenario2);
-  free(cenario3);
-  free(cenario4);
-  free(cenario5);
-  free(cenario6);
-  free(cenario7);
-  free(cenario8);
-  free(cenario9);
-  free(cenario10);
-  free(cenario11);
-  free(cenario12);
+  
+  for(int i = 0; i<qntDeCenarios; i++)//liberando memória dos cenários
+  {
+    free(cenarios[i]);
+  }
+  free(cenarios);
+  
   closegraph();
   return 0; 
 }
@@ -402,7 +588,6 @@ void DesenhandoBotao( int xImagem, int yImagem, void *sprites,void *spritesMasca
 void LidandoComFases(void *cenario, int &numItensColetados, int &fase, bool fasePraCima, bool fasePraBaixo, bool fasePraDireita, bool fasePraEsquerda, bool inventario, int blocoInicial, int blocoFinal, int InvIndexInicial, int InvIndexFinal)
 {
   int controleItensInventario = InvAltura/(InvIndexFinal - InvIndexInicial + 1);//Criando variável para controlar a localidade de cada item no inventário.
-  int salvandoPosicaoInicial;
   
   //Desenhando Cenário
   putimage(0, 0, cenario, COPY_PUT);
@@ -464,12 +649,12 @@ void LidandoComFases(void *cenario, int &numItensColetados, int &fase, bool fase
     {
       if(!blocosColisao[i].coletado)//abrindo o inventário
       {
-        blocosColisao[i].x = xInv - blocosColisao[i].largura;
+        blocosColisao[i].x = xInv - blocosColisao[i].largura;//mudando a posição do ícone do inventário
         blocosColisao[i].coletado = true;
 	  }
 	  else//fechando o inventário
 	  {
-	    blocosColisao[i].x = 1280 - blocosColisao[i].largura;
+	    blocosColisao[i].x = 1280 - blocosColisao[i].largura;//reajustando a posição do ícone do inventário
 	    blocosColisao[i].coletado = false;
 	  }
 	  blocosColisao[i].cliqueMouse = false;
@@ -482,9 +667,9 @@ void LidandoComFases(void *cenario, int &numItensColetados, int &fase, bool fase
       setfillstyle(1,RGB(50, 125, 255));
       bar(blocosColisao[i].x + blocosColisao[i].largura, 0, 1280, 720);//desenhando o inventário
       
-      for(int j = InvIndexInicial; j < InvIndexFinal + 1; j++)
+      for(int j = InvIndexInicial; j < InvIndexFinal + 1; j++)//pegando quais itens tem pela fase para aparecer no inventário
       {
-	    if(!blocosColisao[j].coletado)//modificando a imagem c base se o item foi coletado ou não
+	    if(!blocosColisao[j].coletado)//modificando a imagem com base se o item foi coletado ou não
 	    {
 	      setfillstyle(1,RGB(50, 125, 50));
 	      bar(xInv + (InvLargura/2) - (blocosColisao[i].largura/2), controleItensInventario*(j - InvIndexInicial + 1) - (controleItensInventario/2) - (blocosColisao[j].largura/2), xInv + (InvLargura/2) + (blocosColisao[i].largura/2), controleItensInventario*(j - InvIndexInicial + 1) - (controleItensInventario/2) + (blocosColisao[j].largura/2));
@@ -502,7 +687,7 @@ void LidandoComFases(void *cenario, int &numItensColetados, int &fase, bool fase
   (GetCursorPos(&P));
   (ScreenToClient(janela, &P));
       
-  for(int i = 0; i < qntBlocos; i++)
+  for(int i = 0; i < qntBlocos; i++)//conferindo a colisão para cada Bloco de Colisão
   {
     ChecagemDeColisao(P.x, P.y, blocosColisao[i].x, blocosColisao[i].y, blocosColisao[i].largura, blocosColisao[i].altura, blocosColisao[i].colidido);//Coletando a informação de colisão e guardando dentro do item.
   }
@@ -544,7 +729,6 @@ void LidandoComFases(void *cenario, int &numItensColetados, int &fase, bool fase
 	  }
 	}
   }
-  
 }
 
 bool ChecagemDeColisao( int xColisor, int yColisor, int xColidido, int yColidido, int larguraColidido, int alturaColidido, bool &colidiu)
